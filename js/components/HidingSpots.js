b155ba996d6754985fc9b951dee17f307f641352
dmitri <dmitri@vistarmedia.com>
2015-08-30 22:56:33 -0400

    Working on forward backward pagination

--- js/components/HidingSpots.js
+++ js/components/HidingSpots.js
@@ -10,24 +10,36 @@ class HidingSpots extends React.Component {
     ));
   }
 
-  _handleHidingSpotPageClick() {
-    if (this.props.game.hidingSpots.pageInfo.hasNextPage) {
-      this.props.relay.setVariables({
-        hidingSpotCursor: this.props.game.hidingSpots.pageInfo.endCursor,
-      });
-    } else {
-      this.props.relay.setVariables({
-        hidingSpotCursor: null,
-      });
-    }
+  _handleHidingSpotNextPageClick() {
+    this.props.relay.setVariables({
+      first: 3,
+      afterHidingSpotCursor: this.props.game.hidingSpots.pageInfo.endCursor,
+      last: null,
+      beforeHidingSpotCursor: null,
+    });
+  }
+
+  _handleHidingSpotPreviousPageClick() {
+    this.props.relay.setVariables({
+      first: null,
+      afterHidingSpotCursor: null,
+      last: 3,
+      beforeHidingSpotCursor: this.props.game.hidingSpots.pageInfo.startCursor,
+    });
   }
 
   renderCursorButtons() {
     return (
       <div>
         <button
-          onClick={this._handleHidingSpotPageClick.bind(this)}>
-          Page
+          disabled={!this.props.game.hidingSpots.pageInfo.hasPreviousPage}
+          onClick={this._handleHidingSpotPreviousPageClick.bind(this)}>
+          backward
+        </button>
+        <button
+          disabled={!this.props.game.hidingSpots.pageInfo.hasNextPage}
+          onClick={this._handleHidingSpotNextPageClick.bind(this)}>
+          forward
         </button>
       </div>
     );
@@ -45,12 +57,20 @@ class HidingSpots extends React.Component {
 
 export default Relay.createContainer(HidingSpots, {
   initialVariables: {
-    hidingSpotCursor: null
+    first: 3,
+    afterHidingSpotCursor: null,
+    last: null,
+    beforeHidingSpotCursor: null,
   },
   fragments: {
     game: () => Relay.QL`
       fragment on Game {
-        hidingSpots(first: 3 after: $hidingSpotCursor) {
+        hidingSpots(
+          first: $first
+          after: $afterHidingSpotCursor
+          last:  $last
+          before: $beforeHidingSpotCursor
+        ) {
           edges {
             node {
               ${HidingSpot.getFragment('hidingSpot')},
@@ -59,7 +79,9 @@ export default Relay.createContainer(HidingSpots, {
           },
           pageInfo {
             hasNextPage,
+            hasPreviousPage,
             endCursor,
+            startCursor,
           },
         },
         ${HidingSpot.getFragment('game')},
